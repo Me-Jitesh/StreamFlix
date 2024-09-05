cac87143a9b1f2c091f2a2ab757f88cc29f8c214
Jitesh Singh <jiteshs101@gmail.com>
2024-09-05 11:49:53 +0530

    Service Layer Defined

---
+++ src/main/java/com/jitesh/streamflix/services/VideoMetaService.java
@@ -0,0 +1,16 @@
+package com.jitesh.streamflix.services;
+
+import com.jitesh.streamflix.entities.VideoMeta;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface VideoMetaService {
+    VideoMeta saveVideoMeta(VideoMeta videoMeta, MultipartFile file);
+
+    VideoMeta getVideoMeta(String videoId);
+
+    VideoMeta getVideoMetaByTitle(String videoTitle);
+
+    List<VideoMeta> getAllVideoMetas();
+}
