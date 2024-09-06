b55ecc7d9a5ee6d411f322846d08c3807ffa6574
Jitesh Singh <jiteshs101@gmail.com>
2024-09-06 21:25:15 +0530

    Cross Origin added

--- src/main/java/com/jitesh/streamflix/controllers/VideoController.java
+++ src/main/java/com/jitesh/streamflix/controllers/VideoController.java
@@ -5,16 +5,14 @@ import com.jitesh.streamflix.services.VideoMetaService;
 import com.jitesh.streamflix.utils.ResponseMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/videos")
+@CrossOrigin( "*")
 public class VideoController {
 
     private VideoMetaService vmService;
